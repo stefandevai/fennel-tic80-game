9fddb12d1b1685d1dc8296a77d84af21068597b3
stefandevai <stedevai@gmail.com>
2020-04-09 01:59:52 +0200

    Add script file

--- build.sh
+++ build.sh
@@ -1,29 +1,56 @@
 #!/bin/bash
 
+# Global variables:
+# -----------------
+# SOURCE_FILES:  Fennel source files to be compiled
+# SOURCE_DIR:    Directory to which the lua output file will be placed
+# OUT_FILE:      Name of the output lua file
+# BUILD_DIR:     Directory for the build files
+
+SOURCE_FILES=(player.fnl main.fnl)
+SOURCE_DIR=source
+OUT_FILE=game.lua
+BUILD_DIR=build
+
+# Compiles all source Fennel files to a single Lua file
+# -----------------------------------------------------
+
 compile()
 {
-  echo "Compiling fennel to lua..."
-  cat source/main.fnl >> build/out.fnl
-  fennel --compile build/out.fnl > build/main.lua
+  mkdir -p build
+  echo "Compiling source files (${SOURCE_FILES[*]}) to $OUT_FILE..."
+  echo "" > $BUILD_DIR/out.fnl
+
+  for file in "${SOURCE_FILES[@]}"; do
+    cat $SOURCE_DIR/"$file" >> $BUILD_DIR/out.fnl
+  done
+
+  fennel --compile $BUILD_DIR/out.fnl > $BUILD_DIR/$OUT_FILE
 }
+
+# Runs the compiled lua file with TIC-80
+# --------------------------------------
 
 run()
 {
-  echo "Running with Tic 80..."
+  echo "Running $OUT_FILE with TIC-80..."
+  tic80 $BUILD_DIR/$OUT_FILE
 }
+
+# Parses command line arguments:
+# ------------------------------
+# -c, --compile: compile source files
+# -r, --run:     run compiled file with tic80
 
 while [ "$1" != "" ]; do
   case $1 in
     -c | --compile )
-      shift
       compile
       ;;
     -r | --run )
-      shift
       run
       ;;
     * )
-      usage
       exit 1
   esac
   shift
